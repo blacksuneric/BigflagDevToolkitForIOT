4091c5bf8647e814699a8b1a5bf4e682e1c29a10
eric,liu <34223022@qq.com>
2017-09-29 00:26:02 +0800

    Change the socket sessoin message bean, so it can be serializable

--- Bigflag toolkit/src/main/java/com/bigflag/toolkit/tool/socket/beans/SocketSessionMsgBean.java
+++ Bigflag toolkit/src/main/java/com/bigflag/toolkit/tool/socket/beans/SocketSessionMsgBean.java
@@ -1,77 +1,63 @@
-/**
- * 
- */
 package com.bigflag.toolkit.tool.socket.beans;
+import java.util.Arrays;
+import com.alibaba.fastjson.JSON;
 
-/***
- * 
- * Copyright 2017-2027 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author Eric,Liu<br> 
- *		   mail:     34223022@qq.com<br>
- *         Create at:2017年9月28日 下午3:27:13
- */
-public final class SocketSessionMsgBean {
-	private final long sessionID;
-	private final String senderID;
-	private final byte[] data;
+public class SocketSessionMsgBean {
+	private long sessionID;
+	private String senderID;
+	private byte[] data;
+
+	public static SocketSessionMsgBean newInstance(){
+		return new SocketSessionMsgBean();
+	}
+
+	public void setSessionID(long sessionID){
+		this.sessionID=sessionID;
+	}
+
+	public long getSessionID(){
+		return this.sessionID;
+	}
+
+	public SocketSessionMsgBean sessionID(long sessionID){
+		this.sessionID=sessionID;
+		return this;
+	}
+
+	public void setSenderID(String senderID){
+		this.senderID=senderID;
+	}
+
+	public String getSenderID(){
+		return this.senderID;
+	}
+
+	public SocketSessionMsgBean senderID(String senderID){
+		this.senderID=senderID;
+		return this;
+	}
+
+	public void setData(byte [] data){
+		this.data=data;
+	}
+
+	public byte[] getData(){
+		return this.data;
+	}
+
+	public SocketSessionMsgBean data(byte [] data){
+		this.data=data;
+		return this;
+	}
 	
-	public SocketSessionMsgBean(Builder builder)
+	public String toJson()
 	{
-		this.sessionID=builder.sessionID;
-		this.senderID=builder.senderID;
-		this.data=builder.data;
-	}
-	
-	public static class Builder{
-		private long sessionID;
-		private String senderID;
-		private byte[] data;
-
-		public Builder(long sessionID) {
-			this.sessionID = sessionID;
-		}
-
-		public void setSessionID(long sessionID) {
-			this.sessionID = sessionID;
-		}
-
-		public void setSenderID(String senderID) {
-			this.senderID = senderID;
-		}
-
-		public void setData(byte[] data) {
-			this.data = data;
-		}
-		
-		public SocketSessionMsgBean build()
-		{
-			return new SocketSessionMsgBean(this);
-		}
+		return JSON.toJSONString(this);
 	}
 
-	public long getSessionID() {
-		return sessionID;
-	}
-
-	public String getSenderID() {
-		return senderID;
-	}
-
-	public byte[] getData() {
-		return data;
+	@Override
+	public String toString() {
+		return "SocketSessionMsgBean [sessionID=" + sessionID + ", senderID=" + senderID + ", data=" + Arrays.toString(data) + "]";
 	}
 	
 	
