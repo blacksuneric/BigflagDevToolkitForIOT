056e176c9078076fcd5d9716f66faac18b787fc5
eric,liu <34223022@qq.com>
2017-10-09 21:28:30 +0800

    Add mongodb service interface

---
+++ Bigflag toolkit/src/main/java/com/bigflag/toolkit/db/interfaces/IMongoDBService.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.bigflag.toolkit.db.interfaces;
+
+import java.util.List;
+
+import com.bigflag.toolkit.db.beans.BaseDBBean;
+
+/**
+ * Copyright 2017-2027 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Eric,Liu<br> 
+ *		   mail:     34223022@qq.com<br>
+ *         Create at: 2017年10月9日 下午9:16:12 
+ */
+public interface IMongoDBService {
+	public boolean connectMongoDB(List<String> hosts,String dbName);
+	public boolean upsert(String collectionName,BaseDBBean dbBean);
+}
