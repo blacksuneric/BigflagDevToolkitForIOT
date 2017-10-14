261916fe474fe038803c8c2f8cc47efe6396c7a5
Eric Liu <34223022@qq.com>
2017-10-14 21:41:29 +0800

    Add PRC service for ESB architecture

---
+++ Bigflag toolkit/src/test/java/com/bigflag/toolkit/test/ITestRemote.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.bigflag.toolkit.test;
+
+/**
+ * Copyright 2017-2027 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Eric,Liu<br> 
+ *		   mail:     34223022@qq.com<br>
+ *         Create at:2017年10月14日 下午5:44:51
+ */
+public interface ITestRemote {
+	public int add(int i,int j);
+	public int minus(int i,int j);
+}
