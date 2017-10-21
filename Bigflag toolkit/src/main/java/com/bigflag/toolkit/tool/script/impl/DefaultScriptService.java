231cf43f80ba19a21b1f62f07b50698e24927795
eric,liu <34223022@qq.com>
2017-10-21 14:58:37 +0800

    Add config service and script engine to execute javascript

---
+++ Bigflag toolkit/src/main/java/com/bigflag/toolkit/tool/script/impl/DefaultScriptService.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.bigflag.toolkit.tool.script.impl;
+
+import com.bigflag.toolkit.tool.script.interfaces.IScriptService;
+
+/**
+ * Copyright 2017-2027 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Eric,Liu<br> 
+ *		   mail:     34223022@qq.com<br>
+ *         Create at: 2017年10月21日 下午2:56:57 
+ */
+public class DefaultScriptService implements IScriptService {
+
+	/* (non-Javadoc)
+	 * @see com.bigflag.toolkit.tool.script.interfaces.IScriptService#executeScript(java.lang.String, java.lang.Object[])
+	 */
+	@Override
+	public Object executeScript(String scriptContent, Object... inputValues) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
